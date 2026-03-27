fda1e8ccc5ec3da1ef8f53d7ca26bf05dc1f627e
Ramz <ramzahmedm@gmail.com>
2026-03-27 16:49:04 +0000

    Implemented dishes with 3 options (Pasta,pizza,salad)

--- demoproject/demoapp/urls.py
+++ demoproject/demoapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [ 
     path('', views.index, name='index'), 
+    path('dishes/<str:dish>', views.dishes)
 ] 
--- demoproject/demoapp/views.py
+++ demoproject/demoapp/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import HttpResponse
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello world. This is the index view of the django app!")
+
+def dishes(request, dish):
+    items = {
+        'pasta': 'Pasta is a type of Italian food typically made from wheat flour and water, and sometimes eggs. It comes in various shapes and sizes, such as spaghetti, penne, and fusilli.',
+        'pizza': 'Pizza is a popular Italian dish consisting of a round, flat base of dough topped with tomato sauce, cheese, and various toppings such as vegetables, meats, and herbs.',
+        'salad': 'Salad is a dish typically made with a mixture of raw or cooked vegetables, fruits, and sometimes proteins like meat, cheese, or nuts. It is often served with a dressing or vinaigrette.'
+    }
+
+    description = items[dish]
+
+    return HttpResponse(f"<h2> {dish} </h2>" + description)
