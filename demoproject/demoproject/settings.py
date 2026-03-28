af65f0398bcf5da29b0511bc97f5361909a22305
Ramz <ramzahmedm@gmail.com>
2026-03-28 00:45:22 +0000

    Settings adjusted

--- demoproject/demoproject/settings.py
+++ demoproject/demoproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-ar6sijr_35$98$1oe2e^ib7@=-&m@b%*w(b4acy@95a%g88_4#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
