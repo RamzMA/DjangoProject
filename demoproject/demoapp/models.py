5189bb49e0f32f0c453e04be96114a4965ac346e
Ramz <ramzahmedm@gmail.com>
2026-03-30 19:21:25 +0100

    Created one-many relations for menu

--- demoproject/demoapp/models.py
+++ demoproject/demoapp/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class MenuCategory(models.Model):
+    menu_category_name = models.CharField(max_length=200)
+
+
 class Menu(models.Model):
     name = models.CharField(max_length=100)
-    cuisine = models.CharField(max_length=100)
     price = models.IntegerField()
+    category_id = models.ForeignKey(MenuCategory, on_delete=models.PROTECT, default=None, related_name="category_name", null=True)
 
     def __str__(self):
-        return self.name + " : " + self.cuisine
+        return self.name + " : " + str(self.category_id)
+    
