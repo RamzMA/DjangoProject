c0e3c6222ce54625a6078c2c87a7a5bbe23ea2e7
Ramz <ramzahmedm@gmail.com>
2026-03-31 04:30:48 +0100

    Implemented custom admin

--- demoproject/demoapp/admin.py
+++ demoproject/demoapp/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
-from .models import Menu, MenuCategory, ApplicationForm
+from .models import Menu, MenuCategory, ApplicationForm, Person
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+
+admin.site.unregister(User)
+admin.site.register(Person)
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ("last_name", "first_name")
+    search_fields = ("first_name__startswith", )
+
+@admin.register(User)
+class NewAdmin(UserAdmin):
+    def get_form(self, request, obj, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+    
+        is_supervisor = request.user.is_superuser
+
+        if not is_supervisor:
+            form.base_fields['username'].disabled = True
+
+        return form
+
+
 
 # Register your models here.
 admin.site.register(Menu)
--- demoproject/demoapp/models.py
+++ demoproject/demoapp/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+class Person(models.Model):
+    last_name = models.TextField()
+    first_name = models.TextField()
+
+    def __str__(self):
+        return f"{self.last_name}, {self.first_name}"
+
 class MenuCategory(models.Model):
     menu_category_name = models.CharField(max_length=200)
 
