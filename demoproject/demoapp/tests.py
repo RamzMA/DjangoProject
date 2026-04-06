08e71e72f4dc531fea8b2bf0d4e51939a605e085
Ramz <ramzahmedm@gmail.com>
2026-04-06 01:49:31 +0100

    Implemented:Test case to ensure string format for user form input

--- demoproject/demoapp/tests.py
+++ demoproject/demoapp/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import ApplicationForm
 
 # Create your tests here.
+class ApplicationTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.application = ApplicationForm.objects.create(
+            name="Test User",
+            email="testuser@example.com",
+            drinksChoice="coffee"
+        )
+
+    def test_fields(self):
+        self.assertIsInstance(self.application.name, str)
+        self.assertIsInstance(self.application.email, str)
+        self.assertIsInstance(self.application.drinksChoice, str)
